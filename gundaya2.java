ee9773d7bcf34df13ca6507643b9390722e0b67b
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:13:25 +0800

    Update gundaya2.java

--- gundaya2.java
+++ gundaya2.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class gundaya2
 {
-    public static void problem2() {
+    public static void main(String [] args) {
         int[] arr = {7, 3, 7, 4, 7, 2, 1, 7, 9, 7};
         int count = 0;
  
