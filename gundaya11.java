b2d8eac034b9c04353fb1599712ba884fadc9789
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:17:33 +0800

    Update gundaya11.java

--- gundaya11.java
+++ gundaya11.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class gundaya11
 {
-     public static void problem11() {
+     public static void main(String [] args) {
         int[] arr = {10, 20, 30, 40, 50};
  
         for (int i = 0; i < arr.length; i += 2)
