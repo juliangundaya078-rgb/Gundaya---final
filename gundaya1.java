4ea489bb94da41c2960c37f00b647338b94e3eae
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:12:43 +0800

    Update gundaya1.java

--- gundaya1.java
+++ gundaya1.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class gundaya1
 
 {
-      public static void problem1(Scanner in) {
+      public static void main(String [] args) {
         int num;
         do {
             System.out.print("Enter a number (1-100): ");
