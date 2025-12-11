3fd91c65da23128889630e240a4c6d92a55e3e47
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:18:23 +0800

    Update gundaya14.java

--- gundaya14.java
+++ gundaya14.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class gundaya14
 {
-    public static void problem14() {
+    public static void main(String [] args) {
         int[] arr = {1, 2, 3, 4, 5};
  
         for (int i = arr.length - 1; i >= 0; i--)
