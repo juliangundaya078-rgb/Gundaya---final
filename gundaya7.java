66290756aec88fa22195c0883f7cefbb1be5bac1
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:15:52 +0800

    Update gundaya7.java

--- gundaya7.java
+++ gundaya7.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class gundaya7
 {
-    public static void problem7() {
+    public static void main(String [] args) {
         int[] arr = {-5, 3, -2, 8, -9};
  
         for (int i = 0; i < arr.length; i++)
