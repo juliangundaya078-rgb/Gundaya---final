c630a9431eac707b3cd81587d02fd2961eb1a2b5
juliangundaya078-rgb <juliangundaya078@gmail.com>
2025-12-11 10:16:16 +0800

    Update gundaya8.java

--- gundaya8.java
+++ gundaya8.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class gundaya8
 {
-      public static void problem8() {
+      public static void main(String [] args) {
         int[] A = {1, 2, 3, 4, 5};
         int[] B = new int[A.length];
  
